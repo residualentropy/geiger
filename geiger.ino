33685b2b4f53abd7e9e7295e6f4f6e60afed8f5e
Landon <161914158+residualentropy@users.noreply.github.com>
2024-09-21 22:38:20 -0500

    Add LEDs and allow disabling piezo & LCD

--- geiger.ino
+++ geiger.ino
@@ -4,9 +4,14 @@
 
 #include <LiquidCrystal.h>
 
+const bool LCD_ENABLE = false;
+const bool PIEZO_ENABLE = true;
+
 const int PIEZO = 8;
 const int HVTRIG = 7;
 const int RESP = 2;
+
+const int LED_SENT = A1;
 
 const int TIME = 700;// 219; // 219 @ 3V, 145 @ 5V
 
@@ -87,11 +92,15 @@ void setup() {
   pinMode(HVTRIG, OUTPUT);
   pinMode(RESP, INPUT);
   attachInterrupt(digitalPinToInterrupt(RESP), isr_resp, RISING);
-  lcd.createChar(0, LCDBG_ONE);
-  lcd.createChar(1, LCDBG_TWO);
-  lcd.createChar(2, LCDBG_THREE);
-  lcd.createChar(3, LCDBG_FOUR);
-  lcd.createChar(4, LCDBG_GTFOUR);
+  if (LCD_ENABLE) {
+    lcd.createChar(0, LCDBG_ONE);
+    lcd.createChar(1, LCDBG_TWO);
+    lcd.createChar(2, LCDBG_THREE);
+    lcd.createChar(3, LCDBG_FOUR);
+    lcd.createChar(4, LCDBG_GTFOUR);
+  }
+  pinMode(LED_SENT, OUTPUT);
+  Serial.begin(38400);
 }
 
 void loop() {
@@ -104,7 +113,7 @@ void loop() {
   }
   int countsNow = counts;
   if (countsNow > countsBeforeLastBeep) {
-    tone(PIEZO, 740, 10);
+    if (PIEZO_ENABLE) tone(PIEZO, 740, 10);
     countsBeforeLastBeep = countsNow;
   }
   if ((millis() - cpsTimeBefore) >= 1000) {
@@ -115,6 +124,10 @@ void loop() {
     rollingCps[0] = cpsNow;
     cpsCountsBefore = countsNow;
     cpsTimeBefore = millis();
+    if (cpsNow > 0) {
+      Serial.println(cpsNow);
+      tone(LED_SENT, 740, 10);
+    }
   }
   int clicksInCpmInterval = 0;
   for (int i = 0; i < CPM_INTERVAL_SECS; i++) {
@@ -122,53 +135,59 @@ void loop() {
   }
   double cpmNow = clicksInCpmInterval / CPM_INTERVAL_MINS;
   lcd.clear();
-  lcd.setCursor(0, 0);
-  for (int i = 0; i < CPM_INTERVAL_SECS; i++) {
-    int x = rollingCps[i];
-    if (x == 0) {
-      lcd.print(" ");
-    } else if (x > 4) {
-      lcd.write(byte(4));
-    } else {
-      lcd.write(byte(x - 1));
-    }
+  if ((!LCD_ENABLE) && (millis() < 3000)) {
+    lcd.setCursor(0, 0);
+    lcd.print("LCD DISABLED!");
   }
-  //lcd.print("Total: ");
-  //lcd.print(countsNow);
-  lcd.setCursor(0, 1);
-  lcd.print((int)cpmNow);
-  lcd.print(" CPM");
-  if ((millis() / 1000) <= CPM_INTERVAL_SECS) {
-    lcd.print(" (WAIT!)");
-  } else {
-    switch ((millis() / 1000) % 6) {
-      case 0:
-        lcd.setCursor(16 - 6, 1);
-        lcd.print("SBM-20");
-        break;
-      case 1:
-        lcd.setCursor(16 - 4, 1);
-        lcd.print("400V");
-        break;
-      case 2:
-        lcd.setCursor(16 - 5, 1);
-        lcd.print("4.7M");
-        lcd.print((char)244);
-        break;
-      case 3:
-        lcd.setCursor(16 - FREQ_KHZ_DIGITS - 3, 1);
-        lcd.print(FREQ_KHZ);
-        lcd.print("KHz");
-        break;
-      case 4:
-        lcd.setCursor(16 - 5, 1);
-        lcd.print("@10mH");
-        break;
-      case 5:
-        lcd.setCursor(16 - CPM_INTERVAL_SECS_DIGITS - 1, 1);
-        lcd.print(CPM_INTERVAL_SECS);
-        lcd.print("s");
-        break;
+  if (LCD_ENABLE) {
+    lcd.setCursor(0, 0);
+    for (int i = 0; i < CPM_INTERVAL_SECS; i++) {
+      int x = rollingCps[i];
+      if (x == 0) {
+        lcd.print(" ");
+      } else if (x > 4) {
+        lcd.write(byte(4));
+      } else {
+        lcd.write(byte(x - 1));
+      }
+    }
+    //lcd.print("Total: ");
+    //lcd.print(countsNow);
+    lcd.setCursor(0, 1);
+    lcd.print((int)cpmNow);
+    lcd.print(" CPM");
+    if ((millis() / 1000) <= CPM_INTERVAL_SECS) {
+      lcd.print(" (WAIT!)");
+    } else {
+      switch ((millis() / 1000) % 6) {
+        case 0:
+          lcd.setCursor(16 - 6, 1);
+          lcd.print("SBM-20");
+          break;
+        case 1:
+          lcd.setCursor(16 - 4, 1);
+          lcd.print("400V");
+          break;
+        case 2:
+          lcd.setCursor(16 - 5, 1);
+          lcd.print("4.7M");
+          lcd.print((char)244);
+          break;
+        case 3:
+          lcd.setCursor(16 - FREQ_KHZ_DIGITS - 3, 1);
+          lcd.print(FREQ_KHZ);
+          lcd.print("KHz");
+          break;
+        case 4:
+          lcd.setCursor(16 - 5, 1);
+          lcd.print("@10mH");
+          break;
+        case 5:
+          lcd.setCursor(16 - CPM_INTERVAL_SECS_DIGITS - 1, 1);
+          lcd.print(CPM_INTERVAL_SECS);
+          lcd.print("s");
+          break;
+      }
     }
   }
 }
